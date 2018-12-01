6f7be3b329f4b1e8beab7b1f1304bb51337639f6
Tomasz <tobomabas@gmail.com>
2018-12-01 15:28:34 +0100

    [change] - test

--- src/main/java/pl/tkarolczyk/new_git_app/Animal.java
+++ src/main/java/pl/tkarolczyk/new_git_app/Animal.java
@@ -3,4 +3,10 @@ package pl.tkarolczyk.new_git_app;
 public class Animal {
  //   public Animal(String name) {
   //  }
+
+
+    @Override
+    public String toString() {
+        return "Animal{}";
+    }
 }
--- src/main/java/pl/tkarolczyk/new_git_app/Cat.java
+++ src/main/java/pl/tkarolczyk/new_git_app/Cat.java
@@ -3,7 +3,7 @@ package pl.tkarolczyk.new_git_app;
 public class Cat extends Animal {
 
     public  Cat(String name){
-        super(name);
+       // super(name);
     }
 
 
